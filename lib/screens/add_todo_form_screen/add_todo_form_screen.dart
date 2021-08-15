2f24752232fa25e066aec7ff42ffb6b65c4e4a6d
Toshiuuki Sato <toshiykst@gmail.com>
2021-08-15 18:16:44 +0900

    Use HookConsumerWidget for add todo form screen

--- lib/screens/add_todo_form_screen/add_todo_form_screen.dart
+++ lib/screens/add_todo_form_screen/add_todo_form_screen.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_todo_app/repositories/todo_repository.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AddTodoFormScreen extends StatefulWidget {
-  const AddTodoFormScreen({Key? key}) : super(key: key);
-
-  @override
-  _AddTodoFormScreenState createState() => _AddTodoFormScreenState();
-}
-
-class _AddTodoFormScreenState extends State<AddTodoFormScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String _title = '';
-  String _description = '';
+class AddTodoFormScreen extends HookConsumerWidget {
+  const AddTodoFormScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _formKey = useMemoized(() => GlobalKey<FormState>());
+    final _titleController = useTextEditingController();
+    final _descriptionController = useTextEditingController();
+
+    final _handleSubmit = useCallback(() async {
+      if (!_formKey.currentState!.validate()) {
+        return;
+      }
+      _formKey.currentState?.save();
+      try {
+        await TodoRepository()
+            .postTodo(_titleController.text, _descriptionController.text);
+      } catch (e) {
+        print(e.toString());
+        return;
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            backgroundColor: Colors.green,
+            content: Row(children: [
+              const Icon(Icons.check_circle, color: Colors.white),
+              const SizedBox(width: 10),
+              const Text('added todo')
+            ])),
+      );
+      Navigator.of(context).pop(true);
+    }, [_formKey]);
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Add todo'),
@@ -25,11 +48,7 @@ class _AddTodoFormScreenState extends State<AddTodoFormScreen> {
                 padding: const EdgeInsets.all(10),
                 child: Column(children: <Widget>[
                   TextFormField(
-                    onSaved: (value) {
-                      if (value != null) {
-                        _title = value;
-                      }
-                    },
+                    controller: _titleController,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter some text';
@@ -41,12 +60,8 @@ class _AddTodoFormScreenState extends State<AddTodoFormScreen> {
                   ),
                   const SizedBox(height: 10),
                   TextFormField(
+                    controller: _descriptionController,
                     maxLines: 10,
-                    onSaved: (value) {
-                      if (value != null) {
-                        _description = value;
-                      }
-                    },
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter some text';
@@ -59,29 +74,7 @@ class _AddTodoFormScreenState extends State<AddTodoFormScreen> {
                   Center(
                       child: ElevatedButton(
                           child: const Text('Submit'),
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              _formKey.currentState?.save();
-                              try {
-                                await TodoRepository()
-                                    .postTodo(_title, _description);
-                              } catch (e) {
-                                print(e.toString());
-                                return;
-                              }
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                    backgroundColor: Colors.green,
-                                    content: Row(children: [
-                                      const Icon(Icons.check_circle,
-                                          color: Colors.white),
-                                      const SizedBox(width: 10),
-                                      const Text('added todo')
-                                    ])),
-                              );
-                              Navigator.of(context).pop(true);
-                            }
-                          }))
+                          onPressed: _handleSubmit))
                 ]))));
   }
 }
